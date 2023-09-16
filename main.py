3a665dda90aaf3b9035cb10b899ed5e04cf9f680
luk <luk.imperinetti@gmail.com>
2023-09-17 00:08:39 +0200

    Dict creation, refacto & big optimisation

--- main.py
+++ main.py
@@ -1,124 +1,77 @@
-# -------- Initialize the data --------#
+# Données sur les monstres et les attaques
 monsters = {
     'pythachu': {
-        'name' : 'Pythachu',
-        'attacks' : ['tonnerre', 'charge'],
+        'name': 'Pythachu',
+        'attacks': ['tonnerre', 'charge'],
     },
     'pythard': {
-        'name' : 'Pytard',
-        'attacks' : ['jet-de-flotte', 'charge'],
+        'name': 'Pytard',
+        'attacks': ['jet-de-flotte', 'charge'],
     },
     'ponytha': {
-        'name' : 'Ponytha',
-        'attacks' : ['brûlure', 'charge'],
+        'name': 'Ponytha',
+        'attacks': ['brûlure', 'charge'],
     },
 }
 
 attacks = {
-    'charge' : {'damages' : 20},
-    'tonerre' : {'damages' : 50},
-    'charge' : {'damages' : 40},
-    'charge' : {'damages' : 40},
+    'charge': {'damages': 20},
+    'tonnerre': {'damages': 50},
+    'jet-de-flotte': {'damages': 40},
+    'brûlure': {'damages': 40},
 }
 
-# create a list to store the players
-players = []
-
-# -------- Initialize the game --------#
+# Fonction pour saisir le nom et les PV d'un joueur
 def saisie_nom_et_pv(player_name):
     while True:
         nom = input(f'Entrez le nom du {player_name} joueur : ').capitalize()
         pv = input('Et son nombre de PV : ')
         if pv.isdigit() and int(pv) > 0:
-            print('Monstres disponibles : ')
-            for monster in monsters.values():
-                print('- ', monster['name'])
-            monster_name = input('Choisissez votre monstre : ').lower()
-            if monster_name not in monsters.keys():
-                print('Monstre invalide')            
-            
-            return nom, int(pv), monster_name
+            return nom, int(pv)
         else:
             print('Nombre de PV invalide (doit être un nombre positif)')
-        
-        
 
+# Fonction pour effectuer une attaque
+def effectuer_attaque(attacker, defender):
+    print(attacker['name'] + ', quelle attaque voulez-vous utiliser ?')
+    for i, attack_name in enumerate(attacker['monster']['attacks'], 1):
+        print(f'{i}: {attack_name.capitalize()} ({attacks[attack_name]["damages"]} PV)')
+    
+    while True:
+        choice = input('> ')
+        if choice.isdigit() and 1 <= int(choice) <= len(attacker['monster']['attacks']):
+            attack_name = attacker['monster']['attacks'][int(choice) - 1]
+            damages = attacks[attack_name]['damages']
+            defender['pv'] -= damages
+            print(f'{attacker["name"]} attaque {defender["name"]} qui perd {damages} PV.')
+            print(f'{defender["name"]} a maintenant {defender["pv"]} PV.')
+            break
+        else:
+            print('Attaque invalide, veuillez ressaisir le numéro.')
 
-player_1, pv_1, monster_1 = saisie_nom_et_pv("1er")
-dict_p_1 = {'name': player_1, 'pv': pv_1, 'monster': monster_1}
-player_2, pv_2, monster_2 = saisie_nom_et_pv("2ème")
-dict_p_2 = {'name': player_2, 'pv': pv_2, 'monster': monster_2}
+# Initialisation des joueurs
+players = []
+for i in range(2):
+    player_name, player_pv = saisie_nom_et_pv(f'{i + 1}er') 
+    monster_name = input('Choisissez votre monstre parmi ' + ', '.join(monsters.keys()) + ' : ')
+    if monster_name in monsters:
+        player = {
+            'name': player_name,
+            'pv': player_pv,
+            'monster': monsters[monster_name]
+        }
+        players.append(player)
+    else:
+        print('Monstre invalide. Réessayez.')
 
-players.append(dict_p_1)
-players.append(dict_p_2)
+# Boucle de combat
+while all(player['pv'] > 0 for player in players): # all() vérifie que tous les joueurs ont encore des PV (return bool)
+    for i, player in enumerate(players):
+        other_player = players[1 - i]  # L'autre joueur
+        effectuer_attaque(player, other_player)
 
-print(players)
-
-init = f"+ {player_1} ({pv_1}) affronte {player_2} ({pv_2}) +"
-def afficher_cadre(texte):
-    cadre = "+" * len(texte)
-    print(f"{cadre}\n{texte}\n{cadre}\n\n")
-afficher_cadre(init)
-
-
-# -------- Choose your monster --------#
-
-
-
-
-# -------- Round 1 Player 1 attack --------#
-
-while int(pv_1) > 0 and int(pv_2) > 0:
-    attack_names = ['charge', 'tonnerre']
-    attack_damages = [20, 50]
-    print(player_1 + ' Quelle attaque voulez-vous utiliser ? \n')
-    i = 1
-    for name in attack_names:
-        print(i, name.capitalize(), -attack_damages[i - 1], ' PV')
-        i += 1
-    att1 = input('> ')
-    while not att1.isdigit() or not 1 <= int(att1) <= len(attack_names):  # tant que mon input n'est pas un int, ou pas comprit entre 1 et le nombre d'index de attaque_name (on a nommé les choix 1 ou 2...)
-        print('Attaque invalide, veuillez resaisir le numéro')
-        att1 = input('> ')
-    att1_idx = int(att1) - 1
-    damages = attack_damages[att1_idx]
-    action_p_1 = f"+ {player_1} attaque {player_2} qui perd {damages} PV +"
-    pv_2 = int(pv_2 - damages)
-    res_punch_p_1 = f"+ {player_2} a maintenant {pv_2} PV"
-    afficher_cadre(action_p_1)
-    afficher_cadre(res_punch_p_1)
-
-    # -------- Round 1 Player 2 attack --------#
-
-    if int(pv_2) > 0:
-        print(player_2 + ' Quelle attaque voulez-vous utiliser ? \n')
-        i = 1
-        for name in attack_names:
-            print(i, name.capitalize(), -attack_damages[i - 1], ' PV')
-            i += 1
-        att2 = input('> ')
-        while not att2.isdigit() or not 1 <= int(att2) <= len(
-                attack_names):  # tant que mon input n'est pas un int, ou pas comprit entre 1 et le nombre d'index de attaque_name (on a nommé les choix 1 ou 2...)
-            print('Attaque invalide, veuillez resaisir le numéro')
-            att2 = input('> ')
-        att2_idx = int(att2) - 1
-        damages2 = attack_damages[att2_idx]
-        action_p_2 = f"+ {player_2} attaque {player_1} qui perd {str(damages2)} PV +"
-        pv_1 = int(pv_1 - damages2)
-        res_punch_p_2 = f"+ {player_1} a maintenant {str(pv_1)} PV+"
-        afficher_cadre(action_p_2)
-        afficher_cadre(res_punch_p_2)
-
-    # -------- Round 1 result --------#
-
-    if int(pv_1) <= 0 or int(pv_2) <= 0:
-        print('La partie est terminée')
-        result_line = "+ Résultat du combat : +"
-        total_p_1 = f"+ {player_1} a {str(pv_1)} PV"
-        total_p_2 = f"+ {player_2} a {str(pv_2)} PV"
-        cadre_result = ""
-        cadre_result = "+" * len(result_line)
-        total_p_1 = f"+ {player_1} a {pv_1} PV".ljust(len(cadre_result) - 1, ' ') + "+" # ici une autre méthode pour créer le cadre sans avoir besoin de faire une fonction !
-        total_p_2 = f"+ {player_2} a {pv_2} PV".ljust(len(cadre_result) - 1, ' ') + "+"
-        print(cadre_result + '\n' + result_line + "\n" + total_p_1 + "\n" + total_p_2 + "\n" + cadre_result + '\n\n')
-        break
+# Affichage du résultat
+print('La partie est terminée')
+print('+ Résultat du combat +')
+for player in players:
+    print(f'{player["name"]} a {player["pv"]} PV')
